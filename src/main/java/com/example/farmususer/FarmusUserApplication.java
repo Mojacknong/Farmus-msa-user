04c65bd54f30d96bdd414d81bde70f2cd92f302e
MinchoGreenT <dldmstjq99@gmail.com>
2023-10-12 19:06:55 +0900

    initial commit

---
+++ src/main/java/com/example/farmususer/FarmusUserApplication.java
@@ -0,0 +1,13 @@
+package com.example.farmususer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FarmusUserApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FarmusUserApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/farmususer/FarmusUserApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.farmususer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FarmusUserApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
