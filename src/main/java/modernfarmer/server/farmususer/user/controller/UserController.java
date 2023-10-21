9f305395b03c3bbd063c48ad8e8b8820c28b3f95
MinchoGreenT <dldmstjq99@gmail.com>
2023-10-21 15:53:03 +0900

    [fix] 재발급, 로그아웃 컨트롤러 변경

---
+++ src/main/java/modernfarmer/server/farmususer/user/controller/UserController.java
@@ -0,0 +1,51 @@
+package modernfarmer.server.farmususer.user.controller;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import modernfarmer.server.farmususer.user.dto.response.ResponseDto;
+import modernfarmer.server.farmususer.user.dto.response.TokenResponseDto;
+import modernfarmer.server.farmususer.user.service.AuthService;
+import modernfarmer.server.farmususer.user.util.JwtTokenProvider;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.servlet.http.HttpServletRequest;
+
+@Slf4j
+@RestController
+@RequestMapping("/api/user")
+@RequiredArgsConstructor
+public class UserController {
+
+    private final AuthService authService;
+    private final JwtTokenProvider jwtTokenProvider;
+
+
+    @DeleteMapping("/logout")
+    public ResponseDto logout(HttpServletRequest request)  {
+
+
+        String userId = jwtTokenProvider.getUserId(request);
+
+        ResponseDto logoutResponseDto = authService.logout(Long.valueOf(userId));
+
+        log.info("로그아웃 완료");
+
+        return logoutResponseDto;
+    }
+
+    @GetMapping(value = "/reissue-token")
+    public TokenResponseDto reissueToken(HttpServletRequest request)  {
+
+        String userId = jwtTokenProvider.getUserId(request);
+        String refreshToken = jwtTokenProvider.resolveToken(request);
+
+        TokenResponseDto reissueTokenResponseDto = authService.reissueToken(refreshToken, Long.valueOf(userId));
+
+        log.info("토큰 재발급 완료");
+
+        return reissueTokenResponseDto;
+    }
+}
