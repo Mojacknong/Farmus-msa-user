dc83598091b670080918296a07894ea91922b8d1
seungryeol <opop0421@naver.com>
2023-11-02 17:43:51 +0900

    [fix] BaseResponse 에러처리 isSuccess

--- src/main/java/modernfarmer/server/farmususer/global/exception/success/SuccessMessage.java
+++ src/main/java/modernfarmer/server/farmususer/global/exception/success/SuccessMessage.java
@@ -7,15 +7,15 @@ import static org.springframework.http.HttpStatus.OK;
 
 @Getter
 public enum SuccessMessage {
-    SUCCESS(OK, true, "요청에 성공하였습니다.");
+    SUCCESS(OK, "요청에 성공하였습니다.");
 
     private final int code;
-    private final boolean isSuccess;
+  //  private final boolean isSuccess;
     private final String message;
 
-    SuccessMessage(HttpStatus code, boolean isSuccess, String message) {
+    SuccessMessage(HttpStatus code,  String message) {
         this.code = code.value();
-        this.isSuccess = isSuccess;
+  //      this.isSuccess = isSuccess;
         this.message = message;
     }
 
--- src/main/java/modernfarmer/server/farmususer/user/dto/response/BaseResponseDto.java
+++ src/main/java/modernfarmer/server/farmususer/user/dto/response/BaseResponseDto.java
@@ -14,11 +14,11 @@ import static modernfarmer.server.farmususer.global.exception.success.SuccessMes
 
 
 @Getter
-@JsonPropertyOrder({"code", "isSuccess", "message", "result"})
+@JsonPropertyOrder({"code", "message", "result"})
 public class BaseResponseDto<T> {
     private final int code;
-    @JsonProperty("isSuccess")
-    private final Boolean isSuccess;
+   // @JsonProperty("isSuccess")
+  //  private final Boolean isSuccess;
     private final String message;
 
     @JsonInclude(JsonInclude.Include.NON_NULL)
@@ -27,20 +27,24 @@ public class BaseResponseDto<T> {
     // 요청에 성공한 경우
     public BaseResponseDto(T result) {
         this.code = HttpStatus.OK.value();
-        this.isSuccess = SUCCESS.isSuccess();
+       // this.isSuccess = SUCCESS.isSuccess();
         this.message = SUCCESS.getMessage();
         this.result = result;
     }
 
     public BaseResponseDto(ErrorMessage errorMessage) {
         this.code = errorMessage.getCode();
-        this.isSuccess = errorMessage.isSuccess();
+    //    this.isSuccess = errorMessage.isSuccess();
         this.message = errorMessage.getMessage();
     }
 
     public BaseResponseDto(int code, Boolean isSuccess, String errorMessage) {
         this.code = code;
-        this.isSuccess = isSuccess;
+      //  this.isSuccess = isSuccess;
         this.message = errorMessage;
+    }
+
+    public static <T> BaseResponseDto of(T result){
+        return new BaseResponseDto(result);
     }
 }
