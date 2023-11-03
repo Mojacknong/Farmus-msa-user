8c443550fc02a7e354889aea0f5a035efd315be4
seungryeol <opop0421@naver.com>
2023-11-03 17:12:37 +0900

    [fix] 응답 data 변경

--- src/main/java/modernfarmer/server/farmususer/user/dto/response/BaseResponseDto.java
+++ src/main/java/modernfarmer/server/farmususer/user/dto/response/BaseResponseDto.java
@@ -11,20 +11,20 @@ import modernfarmer.server.farmususer.global.exception.success.SuccessMessage;
 
 @Getter
 @Builder
-@JsonPropertyOrder({"code", "message", "result"})
+@JsonPropertyOrder({"code", "message", "data"})
 public class BaseResponseDto<T> {
     private final int code;
     private final String message;
 
     @JsonInclude(JsonInclude.Include.NON_NULL)
-    private T result;
+    private T data;
 
-    public static <T> BaseResponseDto of(SuccessMessage successMessage, T result){
+    public static <T> BaseResponseDto of(SuccessMessage successMessage, T data){
 
         return BaseResponseDto.builder()
                 .code(successMessage.getCode())
                 .message(successMessage.getMessage())
-                .result(result)
+                .data(data)
                 .build();
     }
 
