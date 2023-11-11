65b0f3f78e2517a6077e093c4285a6387c8b8821
seungryeol <opop0421@naver.com>
2023-11-11 23:42:40 +0900

    [fix] 온보딩 난이도 측정 함수 수정

--- src/main/java/modernfarmer/server/farmususer/user/service/OnBoardingService.java
+++ src/main/java/modernfarmer/server/farmususer/user/service/OnBoardingService.java
@@ -62,39 +62,38 @@ public class OnBoardingService {
 
     private String recommendAlgorithms(int time,String skill) {
         boolean isIntermediate = false;
-        boolean isAdvanced = false;
-        boolean isExperienced = false;
+        boolean isMaster = false;
+        boolean isElementary = false;
         boolean isBeginner = false;
 
 
         if ("홈파밍 중급".equals(skill)) {
             isIntermediate = true;
         } else if ("홈파밍 고수".equals(skill)) {
-            isAdvanced = true;
+            isMaster = true;
         } else if ("홈파밍 초보".equals(skill)) {
-            isExperienced = true;
+            isElementary = true;
         } else if ("홈파밍 입문".equals(skill)) {
             isBeginner = true;
         }
 
 
-        if (time == 2 && (isIntermediate || isAdvanced)) {
+        if (time == 2 && (isIntermediate || isMaster)) {
             return "HARD";
-        } else if (time == 2 && (isBeginner || isExperienced)) {
+        } else if (time == 2 && (isBeginner || isElementary)) {
             return "NORMAL";
-        } else if (time == 1 && isAdvanced) {
+        } else if (time == 1 && isMaster) {
             return "HARD";
-        } else if (time == 1 && (isIntermediate || isExperienced)) {
+        } else if (time == 1 && (isIntermediate || isElementary)) {
             return "NORMAL";
         } else if (time == 1 && isBeginner) {
             return "EASY";
-        } else if (time == 0 && (isExperienced && isBeginner)) {
-            return "EASY";
-        } else if (time == 0 && isExperienced) {
-            return "NORMAL";
-
-        } else if (time == 0 && isAdvanced) {
+        } else if (time == 0 && isMaster) {
             return "HARD";
+        } else if (time == 0 && isIntermediate) {
+            return "NORMAL";
+        } else if (time == 0 && (isElementary || isBeginner)) {
+            return "EASY";
         }
 
         return "알 수 없음";
